f6720ff5611c6d67d9b6a250f1e34a0e88b6fa6e
nikhil <nikhilsharma270027@gmail.com>
2024-07-07 14:45:22 +0530

    hashhigh&low

--- 1. Learn Basics/Learn Hashing/Find the highest&lowest frequency element.cpp
+++ 1. Learn Basics/Learn Hashing/Find the highest&lowest frequency element.cpp
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void countFreq(int arr[], int n) {
+void countFreq(int arr[], int n)
+{
     map<int, int> freq;
 
     // Count frequencies
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         freq[arr[i]]++;
     }
 
-    for (auto it : freq) {
+    for (auto it : freq)
+    {
         cout << it.first << "->" << it.second << endl;
     }
 
@@ -17,13 +20,16 @@ void countFreq(int arr[], int n) {
     int maxEle = 0, minEle = 0;
 
     // Find elements with highest and lowest frquency
-    for (const auto &pair : freq) {
-        if (pair.second > maxFreq) {
+    for (const auto& pair : freq)
+    {
+        if (pair.second > maxFreq)
+        {
             maxFreq = pair.second;
             maxEle = pair.first;
         }
-        if (pair.second < minFreq) {
-            minFreq = pair.first;
+        if (pair.second < minFreq)
+        {
+            minFreq = pair.second;
             minEle = pair.first;
         }
     }
@@ -31,17 +37,17 @@ void countFreq(int arr[], int n) {
     cout << "The highest frequency element is: " << maxEle << "\n";
     cout << "The lowest frequency element is: " << minEle << "\n";
 }
-int main() {
+int main()
+{
     int arr[] = {10, 5, 10, 15, 10, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
     countFreq(arr, n);
     return 0;
 }
 
-// Output: 
+// Output:
 // 5->2
 // 10->3
 // 15->1
 // The highest frequency element is: 10
 // The lowest frequency element is: 15
-
