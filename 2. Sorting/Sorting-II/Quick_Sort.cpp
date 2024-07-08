59fc8bc763950bb84e28c3c10a54a79ed9eb2399
nikhil <nikhilsharma270027@gmail.com>
2024-07-08 14:43:37 +0530

    add QuickSort

---
+++ 2. Sorting/Sorting-II/Quick_Sort.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+
+    while (i <= j)
+    {
+        while (i <= high && arr[i] <= pivot)
+        {
+            i++;
+        }
+
+        while (j >= low && arr[j] > pivot)
+        {
+            j--;
+        }
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> quickSort(vector<int> arr)
+{
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
+
+int main()
+{
+    vector<int> arr = {4, 6, 2, 5, 7, 9, 1, 3};
+    int n = arr.size();
+    cout << "Before Using quick Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    arr = quickSort(arr);
+    cout << "After Using quick sort: " << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
+
+// Output:
+// Before Using quick Sort: 
+// 4 6 2 5 7 9 1 3 
+
+// After Using quick sort: 
+// 1 2 3 4 5 6 7 9 
