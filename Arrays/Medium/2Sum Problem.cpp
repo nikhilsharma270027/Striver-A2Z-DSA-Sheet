87421e6ee22f14cdacae898aeb5ac7647234d013
nikhil <nikhilsharma270027@gmail.com>
2024-07-22 12:14:49 +0530

    add 2sumproblembetter

--- Arrays/Medium/2Sum Problem.cpp
+++ Arrays/Medium/2Sum Problem.cpp
@@ -51,3 +51,58 @@ int main() {
     return 0;
 }
 // Output: This is the answer for variant 2: [1, 3]
+
+// Better Approach 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mpp; // Map to store the index of each number
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int moreNeeded = target - num; // Calculate the complement
+            if (mpp.find(moreNeeded) != mpp.end()) {
+                // If complement exists in the map, return the indices
+                return { mpp[moreNeeded], i };
+            }
+            // Store the index of the current number
+            mpp[num] = i; 
+            
+            // Print the contents of the map
+            cout << "Map contents after iteration " << i << ": ";
+            for (const auto &pair : mpp) {
+                cout << "{" << pair.first << ": " << pair.second << "} ";
+            }
+            cout << endl;
+        }
+        // Return -1, -1 if no solution is found
+        return { -1, -1 };
+    }
+};
+
+int main() {
+    vector<int> nums = { 2, 6, 5, 8, 10 }; // Example input
+    int target = 14; // Example target
+
+    Solution obj;
+    vector<int> ans = obj.twoSum(nums, target);
+
+    // Print the result
+    cout << "Indices of the two numbers that add up to " << target << " are: ";
+    if (ans[0] != -1) {
+        cout << ans[0] << " and " << ans[1] << endl;
+    } else {
+        cout << "No solution found" << endl;
+    }
+
+    return 0;
+}
+
+// Output:
+// Map contents after iteration 0: {2: 0} 
+// Map contents after iteration 1: {2: 0} {6: 1} 
+// Map contents after iteration 2: {2: 0} {5: 2} {6: 1} 
+// Indices of the two numbers that add up to 14 are: 1 and 3
