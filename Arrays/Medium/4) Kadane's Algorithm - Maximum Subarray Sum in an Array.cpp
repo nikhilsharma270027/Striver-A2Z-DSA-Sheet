5d19f99462c18ca85c0c28b08aacdc6a6470a90a
nikhil <nikhilsharma270027@gmail.com>
2024-07-26 17:48:28 +0530

    add kadanesalgoMaxsubarraysum

---
+++ Arrays/Medium/4) Kadane's Algorithm - Maximum Subarray Sum in an Array.cpp
@@ -0,0 +1,74 @@
+// Brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            // subarray = arr[i.....j]
+            int sum = 0;
+
+            //add all the elements of subarray:
+            for (int k = i; k <= j; k++) {
+                sum += arr[k];
+            }
+            maxi = max(maxi, sum);
+        }
+    }
+
+    return maxi;
+}
+
+int main() {
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
+// Output: The maximum subarray sum is: 6
+
+// Complexity Analysis
+// Time Complexity: O(N3), where N = size of the array.
+// Reason: We are using three nested loops, each running approximately N times.
+
+// Space Complexity: O(1) as we are not using any extra space
+
+
+// Better Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            // current subarray = arr[i.....j]
+            //add the current element arr[j]
+            // to the sum i.e. sum of arr[i...j-1]
+            sum += arr[j];
+
+            maxi = max(maxi, sum); // getting the maximum
+        }
+    }
+    return maxi;
+}
+
+int main() {
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
+// Complexity Analysis
+// Time Complexity: O(N2), where N = size of the array.
+// Reason: We are using two nested loops, each running approximately N times.
+
+// Space Complexity: O(1) as we are not using any extra space.
