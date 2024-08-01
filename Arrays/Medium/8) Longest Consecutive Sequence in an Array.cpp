d78b1f43996a0dbbd0ef7146c36396c98c90ad61
nikhil <nikhilsharma270027@gmail.com>
2024-08-01 22:37:45 +0530

    longconsecutiveArr

---
+++ Arrays/Medium/8) Longest Consecutive Sequence in an Array.cpp
@@ -0,0 +1,46 @@
+// Brute Force Approch
+#include <bits/stdc++.h>
+using namespace std;
+
+bool linearSearch(vector<int>&a, int num) {
+    int n = a.size(); //size of array
+    for (int i = 0; i < n; i++) {
+        if (a[i] == num)
+            return true;
+    }
+    return false;
+}
+int longestSuccessiveElements(vector<int>&a) {
+    int n = a.size(); //size of array
+    int longest = 1;
+    //pick a element and search for its
+    //consecutive numbers:
+    for (int i = 0; i < n; i++) {
+        int x = a[i];
+        int cnt = 1;
+        //search for consecutive numbers
+        //using linear search:
+        while (linearSearch(a, x + 1) == true) {
+            x += 1;
+            cnt += 1;
+        }
+
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
+
+int main() {
+    vector<int> a = {100, 200, 1, 2, 3, 4};
+    int ans = longestSuccessiveElements(a);
+    cout << "The longest consecutive sequence is " << ans << "\n";
+    return 0;
+}
+
+// Output: The longest consecutive sequence is 4.
+
+// Complexity Analysis
+// Time Complexity: O(N2), N = size of the given array.
+// Reason: We are using nested loops each running for approximately N times.
+
+// Space Complexity: O(1), as we are not using any extra space to solve this problem.
