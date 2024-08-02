583e32a6799953165828e848094176f44aa43c75
nikhil <nikhilsharma270027@gmail.com>
2024-08-02 23:03:17 +0530

    longConsecutArrBetter

--- Arrays/Medium/8) Longest Consecutive Sequence in an Array.cpp
+++ Arrays/Medium/8) Longest Consecutive Sequence in an Array.cpp
@@ -44,3 +44,49 @@ int main() {
 // Reason: We are using nested loops each running for approximately N times.
 
 // Space Complexity: O(1), as we are not using any extra space to solve this problem.
+
+// Better Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSuccessiveElements(vector<int>&a) {
+    int n = a.size();
+    if (n == 0) return 0;
+
+    //sort the array:
+    sort(a.begin(), a.end());
+    int lastSmaller = INT_MIN;
+    int cnt = 0;
+    int longest = 1;
+
+    //find longest sequence:
+    for (int i = 0; i < n; i++) {
+        if (a[i] - 1 == lastSmaller) {
+            //a[i] is the next element of the
+            //current sequence.
+            cnt += 1;
+            lastSmaller = a[i];
+        }
+        else if (a[i] != lastSmaller) {
+            cnt = 1; // override
+            lastSmaller = a[i]; // currect element
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
+
+int main() {
+    vector<int> a = {1 ,1 ,1 ,2 ,2 ,2 ,3 ,3 ,4 ,4 ,100 ,100 ,101};
+    int ans = longestSuccessiveElements(a);
+    cout << "The longest consecutive sequence is " << ans << "\n";
+    return 0;
+}
+
+// Output: The longest consecutive sequence is 4.
+
+// Complexity Analysis
+// Time Complexity: O(NlogN) + O(N), N = size of the given array.
+// Reason: O(NlogN) for sorting the array. To find the longest sequence, we are using a loop that results in O(N).
+
+// Space Complexity: O(1), as we are not using any extra space to solve this problem.
