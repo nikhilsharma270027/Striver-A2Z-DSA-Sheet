7c457f509622061000391a700f3df32944fa47c5
nikhil <nikhilsharma270027@gmail.com>
2024-08-04 22:46:48 +0530

    setmatrixzerobetter

--- Arrays/Medium/9) Set Matrix Zeros.cpp
+++ Arrays/Medium/9) Set Matrix Zeros.cpp
@@ -69,3 +69,62 @@ int main() {
 // Another O(N*M) is taken to mark all the cells with -1 as 0 finally.
 
 // Space Complexity: O(1) as we are not using any extra space.
+
+// Better Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+    int row[n] = {0}; // row array
+    int col[m] = {0}; // col array
+
+    // Traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                // mark ith index of row wih 1:
+                row[i] = 1;
+
+                // mark jth index of col wih 1:
+                col[j] = 1;
+            }
+        }
+    }
+
+    // Finally, mark all (i, j) as 0
+    // if row[i] or col[j] is marked with 1.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return matrix;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int n = matrix.size();
+    int m = matrix[0].size();
+    vector<vector<int>> ans = zeroMatrix(matrix, n, m);
+
+    cout << "The Final matrix is: n";
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "n";
+    }
+    return 0;
+}
+
+// Output: The Final matrix is: 1 0 1
+//                              0 0 0
+//                              1 0 1
+// Complexity Analysis
+// Time Complexity: O(2*(N*M)), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+// Reason: We are traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
+
+// Space Complexity: O(N) + O(M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+// Reason: O(N) is for using the row array and O(M) is for using the col array.
