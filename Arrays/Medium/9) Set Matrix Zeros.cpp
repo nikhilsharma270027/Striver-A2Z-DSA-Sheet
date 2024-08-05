14cf1f04970e450bf43aca8fb0358cfcd7279d8f
Nikhil Sharma <109808756+nikhilsharma270027@users.noreply.github.com>
2024-08-05 22:30:52 +0530

    Update 9) Set Matrix Zeros.cpp

--- Arrays/Medium/9) Set Matrix Zeros.cpp
+++ Arrays/Medium/9) Set Matrix Zeros.cpp
@@ -64,6 +64,7 @@ int main() {
 //                              0 0 0
 //                              1 0 1
 // Complexity Analysis
+
 // Time Complexity: O((N*M)*(N + M)) + O(N*M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
 // Reason: Firstly, we are traversing the matrix to find the cells with the value 0. It takes O(N*M). Now, whenever we find any such cell we mark that row and column with -1. This process takes O(N+M). So, combining this the whole process, finding and marking, takes O((N*M)*(N + M)).
 // Another O(N*M) is taken to mark all the cells with -1 as 0 finally.
@@ -127,4 +128,7 @@ int main() {
 // Reason: We are traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
 
 // Space Complexity: O(N) + O(M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+
+// Optimal Solution 
+#include<bits/stdc++.h>
 // Reason: O(N) is for using the row array and O(M) is for using the col array.
