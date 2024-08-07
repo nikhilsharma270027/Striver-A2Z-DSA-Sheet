2b7a4eb00656a5b2ccc24f48f0aee2a7faa158b7
nikhil <nikhilsharma270027@gmail.com>
2024-08-07 23:39:24 +0530

    rotateMatrix90

---
+++ Arrays/Medium/10) Rotate matrix 90 degree.cpp
@@ -0,0 +1,37 @@
+// Brute force Approach
+#include<bits/stdc++.h>
+using namespace std;
+
+vector < vector < int >> rotate(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+    vector < vector < int >> rotated(n, vector < int > (n, 0));
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        rotated[j][n - i - 1] = matrix[i][j];
+    }
+    }
+    return rotated;
+}
+
+int main() {
+    vector < vector < int >> arr;
+    arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector < vector < int >> rotated = rotate(arr);
+    cout << "Rotated Image" << endl;
+    for (int i = 0; i < rotated.size(); i++) {
+    for (int j = 0; j < rotated[0].size(); j++) {
+        cout << rotated[i][j] << " ";
+    }
+    cout << "n";
+    }
+}
+
+// Output: Rotated Image
+//         7 4 1
+//         8 5 2
+//         9 6 3
+
+// Complexity Analysis
+// Time Complexity: O(N*N) to linearly iterate and put it into some other matrix.
+
+// Space Complexity: O(N*N) to copy it into some other matrix.
