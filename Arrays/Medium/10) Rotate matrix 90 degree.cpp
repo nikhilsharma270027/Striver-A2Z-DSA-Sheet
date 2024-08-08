794dfc6672d322ce094035ef225cdbd033f36077
nikhil <nikhilsharma270027@gmail.com>
2024-08-08 23:38:20 +0530

    rotateMatrixOptimal

--- Arrays/Medium/10) Rotate matrix 90 degree.cpp
+++ Arrays/Medium/10) Rotate matrix 90 degree.cpp
@@ -35,3 +35,40 @@ int main() {
 // Time Complexity: O(N*N) to linearly iterate and put it into some other matrix.
 
 // Space Complexity: O(N*N) to copy it into some other matrix.
+
+
+// Optimal Approach
+#include<bits/stdc++.h>
+
+using namespace std;
+void rotateOptimal(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+    //transposing the matrix
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
+    }
+    }
+    //reversing each row of the matrix
+    for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    vector < vector < int >> arr;
+    arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    rotateOptimal(arr);
+    cout << "Rotated Image" << endl;
+    for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+        cout << arr[i][j] << " ";
+    }
+    cout << "n";
+    }
+}
+
+// Complexity Analysis
+// Time Complexity: O(N*N) + O(N*N).One O(N*N) is for transposing the matrix and the other is for reversing the matrix.
+
+// Space Complexity: O(1).
