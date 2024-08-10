40d5eb3a293de9e171210ef807ce3a837dabe636
nikhil <nikhilsharma270027@gmail.com>
2024-08-10 21:51:11 +0530

    subArrCount12Brute

---
+++ Arrays/Medium/12) Count subarrays with given sum.cpp
@@ -0,0 +1,38 @@
+// Brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        for (int j = i; j < n; j++) { // ending index j
+
+            // calculate the sum of subarray [i...j]
+            int sum = 0;
+            for (int K = i; K <= j; K++)
+                sum += arr[K];
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
+// Output: The number of subarrays is: 2
+
+// Complexity Analysis
+// Time Complexity: O(N3), where N = size of the array.
+// Reason: We are using three nested loops here. Though all are not running for exactly N times, the time complexity will be approximately O(N3).
+
+// Space Complexity: O(1) as we are not using any extra space.
