97bd8e161ca02a20c44f2f73d0ec7aef9c8334c8
nikhil <nikhilsharma270027@gmail.com>
2024-08-11 12:48:35 +0530

    countsubArrBetter

--- Arrays/Medium/12) Count subarrays with given sum.cpp
+++ Arrays/Medium/12) Count subarrays with given sum.cpp
@@ -36,3 +36,41 @@ int main() {
 // Reason: We are using three nested loops here. Though all are not running for exactly N times, the time complexity will be approximately O(N3).
 
 // Space Complexity: O(1) as we are not using any extra space.
+
+
+// Better Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        int sum = 0;
+        for (int j = i; j < n; j++) { // ending index j
+            // calculate the sum of subarray [i...j]
+            // sum of [i..j-1] + arr[j]
+            sum += arr[j];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
+// Complexity Analysis
+// Time Complexity: O(N2), where N = size of the array.
+// Reason: We are using two nested loops here. As each of them is running for exactly N times, the time complexity will be approximately O(N2).
+
+// Space Complexity: O(1) as we are not using any extra space.
