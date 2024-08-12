908d138f9e6681a74ee9d285f224be7bb6a1c194
nikhil <nikhilsharma270027@gmail.com>
2024-08-12 21:14:19 +0530

    subArrOptimal

--- Arrays/Medium/12) Count subarrays with given sum.cpp
+++ Arrays/Medium/12) Count subarrays with given sum.cpp
@@ -74,3 +74,44 @@ int main() {
 // Reason: We are using two nested loops here. As each of them is running for exactly N times, the time complexity will be approximately O(N2).
 
 // Space Complexity: O(1) as we are not using any extra space.
+
+// Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    map<int, int> mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += arr[i];
+
+        // Calculate x-k:
+        int remove = preSum - k;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
+}
+
+int main() {
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
+// Complexity Analysis
+// Time Complexity: O(N) or O(N*logN) depending on which map data structure we are using, where N = size of the array.
+// Reason: For example, if we are using an unordered_map data structure in C++ the time complexity will be O(N) but if we are using a map data structure, the time complexity will be O(N*logN). The least complexity will be O(N) as we are using a loop to traverse the array.
+
+// Space Complexity: O(N) as we are using a map data structure.
