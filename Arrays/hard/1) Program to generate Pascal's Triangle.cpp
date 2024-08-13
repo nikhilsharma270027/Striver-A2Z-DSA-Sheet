50b020ab7b62e8f1e4c0657395e06c427adbc957
nikhil <nikhilsharma270027@gmail.com>
2024-08-13 21:45:26 +0530

    ArrHardPascal

---
+++ Arrays/hard/1) Program to generate Pascal's Triangle.cpp
@@ -0,0 +1,36 @@
+// Variation 1
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+int pascalTriangle(int r, int c) {
+    int element = nCr(r - 1, c - 1);
+    return element;
+}
+
+int main() {
+    int r = 5; // row number
+    int c = 3; // col number
+    int element = pascalTriangle(r, c);
+    cout << "The element at position (r,c) is: "
+            << element << "n";
+    return 0;
+}
+                
+// Output: The element at position (r,c) is: 6
+
+// Complexity Analysis
+// Time Complexity: O(c), where c = given column number.
+// Reason: We are running a loop for r times, where r is c-1.
+
+// Space Complexity: O(1) as we are not using any extra space.
