55eeac18d29a59e784ac7b75aca7702a3175fd05
nikhil <nikhilsharma270027@gmail.com>
2024-08-20 09:57:25 +0530

    lowerboundbinary

---
+++ 4. Binary Search [1D, 2D Arrays, Search Space]/Lec 1 BS on 1D Arrays/2) Implement Lower Bound.cpp
@@ -0,0 +1,67 @@
+// Brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= x) {
+            // lower bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+int main() {
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
+
+// Output: The lower bound is the index: 3
+
+// Complexity Analysis
+// Time Complexity: O(N), where N = size of the given array.
+// Reason: In the worst case, we have to travel the whole array. This is basically the time complexity of the linear search algorithm.
+
+// Space Complexity: O(1) as we are using no extra space.
+
+
+// Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
+
+// Complexity Analysis
+// Time Complexity: O(logN), where N = size of the given array.
+// Reason: We are basically using the Binary Search algorithm.
+
+// Space Complexity: O(1) as we are using no extra space
