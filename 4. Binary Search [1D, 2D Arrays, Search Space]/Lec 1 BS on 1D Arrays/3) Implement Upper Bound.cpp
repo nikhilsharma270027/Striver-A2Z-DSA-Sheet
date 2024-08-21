5e9a6b4e2d5f1875b3277542239fcb49f8d554d4
nikhil <nikhilsharma270027@gmail.com>
2024-08-21 21:27:33 +0530

    upperboundandfloor/ceil

---
+++ 4. Binary Search [1D, 2D Arrays, Search Space]/Lec 1 BS on 1D Arrays/3) Implement Upper Bound.cpp
@@ -0,0 +1,68 @@
+// Brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > x) {
+            // upper bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+int main() {
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int n = 6, x = 9;
+    int ind = upperBound(arr, x, n);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}
+
+// Output: The upper bound is the index: 4
+
+// Complexity Analysis
+// Time Complexity: O(N), where N = size of the given array.
+// Reason: In the worst case, we have to travel the whole array. This is basically the time complexity of the linear search algorithm.
+
+// Space Complexity: O(1) as we are using no extra space.
+
+
+
+// Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int n = 6, x = 9;
+    int ind = upperBound(arr, x, n);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}
+
+// Complexity Analysis
+// Time Complexity: O(logN), where N = size of the given array.
+// Reason: We are basically using the Binary Search algorithm.
+
+// Space Complexity: O(1) as we are using no extra space.
---
+++ 4. Binary Search [1D, 2D Arrays, Search Space]/Lec 1 BS on 1D Arrays/4)Search Insert Position.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int searchInsert(vector<int>& arr, int x) {
+    int n = arr.size(); // size of the array
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+
+int main() {
+    vector<int> arr = {1, 2, 4, 7};
+    int x = 6;
+    int ind = searchInsert(arr, x);
+    cout << "The index is: " << ind << "\n";
+    return 0;
+}
+// Output: The index is: 3
+
+// Time Complexity: O(logN), where N = size of the given array.
+// Reason: We are basically using the Binary Search algorithm.
+
+// Space Complexity: O(1) as we are using no extra space.
