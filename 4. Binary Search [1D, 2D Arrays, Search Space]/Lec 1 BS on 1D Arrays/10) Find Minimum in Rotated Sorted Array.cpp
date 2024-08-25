7769af0064b296fbfed870f96805d0763270b4bc
nikhil <nikhilsharma270027@gmail.com>
2024-08-25 23:29:41 +0530

    miniand

---
+++ 4. Binary Search [1D, 2D Arrays, Search Space]/Lec 1 BS on 1D Arrays/10) Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,126 @@
+// Brute Force Approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>& arr) {
+    int n = arr.size(); // size of the array.
+    int mini = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        // Always keep the minimum.
+        mini = min(mini, arr[i]);
+    }
+    return mini;
+}
+
+int main() {
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int ans = findMin(arr);
+    cout << "The minimum element is: " << ans << "\n";
+    return 0;
+}
+// Output: The minimum element is: 0
+
+// Complexity Analysis
+// Time Complexity: O(N), N = size of the given array.
+// Reason: We have to iterate through the entire array to check if the target is present in the array.
+
+// Space Complexity: O(1)
+// Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+
+
+// Solution 2 : Using binary search
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>& arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            ans = min(ans, arr[low]);
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            ans = min(ans, arr[mid]);
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int ans = findMin(arr);
+    cout << "The minimum element is: " << ans << "\n";
+    return 0;
+}
+// Complexity Analysis
+// Time Complexity: O(logN), N = size of the given array.
+// Reason: We are basically using binary search to find the minimum.
+
+// Space Complexity: O(1)
+// Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+
+
+// Further optimization
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>& arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            ans = min(ans, arr[low]);
+            break;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            ans = min(ans, arr[low]);
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            ans = min(ans, arr[mid]);
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int ans = findMin(arr);
+    cout << "The minimum element is: " << ans << "\n";
+    return 0;
+}
+
+// Complexity Analysis
+// Time Complexity: O(logN), N = size of the given array.
+// Reason: We are basically using binary search to find the minimum. 
+
+// Space Complexity: O(1)
+// Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
---
+++ 4. Binary Search [1D, 2D Arrays, Search Space]/Lec 1 BS on 1D Arrays/11) Find out how many times the array has been rotated.cpp
@@ -0,0 +1,95 @@
+// Brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int findKRotation(vector<int> &arr) {
+    int n = arr.size(); //size of array.
+    int ans = INT_MAX, index = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < ans) {
+            ans = arr[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+int main() {
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int ans = findKRotation(arr);
+    cout << "The array is rotated " << ans << " times.\n";
+    return 0;
+}
+// Output: The array is rotated 4 times.
+
+// Complexity Analysis
+// Time Complexity: O(N), N = size of the given array.
+// Reason: We have to iterate through the entire array to check if the target is present in the array.
+
+// Space Complexity: O(1)
+// Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+
+
+
+// Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int findKRotation(vector<int> &arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            if (arr[mid] < ans) {
+                index = mid;
+                ans = arr[mid];
+            }
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return index;
+}
+
+int main() {
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int ans = findKRotation(arr);
+    cout << "The array is rotated " << ans << " times.\n";
+    return 0;
+}
+
+// Output: The array is rotated 4 times.
+
+// Complexity Analysis
+// Time Complexity: O(logN), N = size of the given array.
+// Reason: We are basically using binary search to find the minimum.
+
+// Space Complexity: O(1)
+// Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
