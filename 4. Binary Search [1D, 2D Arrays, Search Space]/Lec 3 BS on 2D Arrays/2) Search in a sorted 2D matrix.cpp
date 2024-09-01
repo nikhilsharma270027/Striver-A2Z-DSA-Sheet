561ef78d69a5c49cee5d5ab6c03dc132a7aca4a5
nikhil <nikhilsharma270027@gmail.com>
2024-09-01 23:23:36 +0530

    search2dmatrix

---
+++ 4. Binary Search [1D, 2D Arrays, Search Space]/Lec 3 BS on 2D Arrays/2) Search in a sorted 2D matrix.cpp
@@ -0,0 +1,104 @@
+// brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size(), m = matrix[0].size();
+
+    //traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == target)
+                return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+
+// Output: true
+
+// Complexity Analysis
+// Time Complexity: O(N X M), where N = given row number, M = given column number.
+// Reason: In order to traverse the matrix, we are using nested loops running for n and m times respectively.
+
+// Space Complexity: O(1) as we are not using any extra space.
+
+
+// Better Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+bool binarySearch(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return true;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    for (int i = 0; i < n; i++) {
+        if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
+            return binarySearch(matrix[i], target);
+        }
+    }
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+
+// Complexity Analysis
+// Time Complexity: O(N + logM), where N = given row number, M = given column number.
+// Reason: We are traversing all rows and it takes O(N) time complexity. But for all rows, we are not applying binary search rather we are only applying it once for a particular row. That is why the time complexity is O(N + logM) instead of O(N*logM).
+
+// Space Complexity: O(1) as we are not using any extra space.
+
+
+// Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    //apply binary search:
+    int low = 0, high = n * m - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int row = mid / m, col = mid % m;
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+
+// Complexity Analysis
+// Time Complexity: O(log(NxM)), where N = given row number, M = given column number.
+// Reason: We are applying binary search on the imaginary 1D array of size NxM.
+
+// Space Complexity: O(1) as we are not using any extra space.
