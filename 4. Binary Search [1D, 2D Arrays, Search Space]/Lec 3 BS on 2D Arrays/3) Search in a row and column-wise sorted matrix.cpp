493242c5a003714e2ede6f6d05aa56342ac3eab1
nikhil <nikhilsharma270027@gmail.com>
2024-09-02 21:31:18 +0530

    searchsortedArr

---
+++ 4. Binary Search [1D, 2D Arrays, Search Space]/Lec 3 BS on 2D Arrays/3) Search in a row and column-wise sorted matrix.cpp
@@ -0,0 +1,110 @@
+// Brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchElement(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size(), m = matrix[0].size();
+
+    //traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == target)
+                return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+
+// Output: true
+
+// Complexity Analysis
+// Time Complexity: O(N X M), where N = given row number, M = given column number.
+// Reason: In order to traverse the matrix, we are using nested loops running for n and m times respectively.
+
+// Space Complexity: O(1) as we are not using any extra space.
+
+
+// Better Approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool binarySearch(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return true;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
+bool searchElement(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+
+    for (int i = 0; i < n; i++) {
+        bool flag =  binarySearch(matrix[i], target);
+        if (flag) return true;
+    }
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+
+// Complexity Analysis
+// Time Complexity: O(N*logM), where N = given row number, M = given column number.
+// Reason: We are traversing all rows and it takes O(N) time complexity. And for all rows, we are applying binary search. So, the total time complexity is O(N*logM).
+
+// Space Complexity: O(1) as we are not using any extra space.
+
+
+// Optimal Approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchElement(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int row = 0, col = m - 1;
+
+    //traverse the matrix from (0, m-1):
+    while (row < n && col >= 0) {
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) row++;
+        else col--;
+    }
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
+
+// Complexity Analysis
+// Time Complexity: O(N+M), where N = given row number, M = given column number.
+// Reason: We are starting traversal from (0, M-1), and at most, we can end up being in the cell (M-1, 0). So, the total distance can be at most (N+M). So, the time complexity is O(N+M).
+
+// Space Complexity: O(1) as we are not using any extra space.
