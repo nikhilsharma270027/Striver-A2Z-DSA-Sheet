3fd32a47e713325b1f7de8c5239159b217c8b531
nikhil <nikhilsharma270027@gmail.com>
2024-09-07 20:58:19 +0530

    insertSummaryinLL

--- 6 . Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 1 Learn 1D LinkedList/insertionSummary.cpp
+++ 6 . Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 1 Learn 1D LinkedList/insertionSummary.cpp
@@ -40,8 +40,106 @@ void print(Node* head) {
     }
 }
 
+// Insert at the head/start
+Node* insertHead(Node* head, int val){
+    Node* temp = new Node(val, head);
+    return temp;
+}
+
+//Insert at the tail/end
+Node* insertTail(Node* head, int val){
+    if(head == NULL) {
+        return new Node(val);
+    }
+    Node* temp = head;
+    while(temp->next != NULL) { 
+        temp = temp->next; // we stop at the last element
+    }
+    Node* newNode = new Node(val);
+    temp->next = newNode;
+    return head;
+}
+
+// insert in Kth position
+Node* insertKPosition(Node* head, int ele, int k ){
+    if(head == NULL){ // empty list
+        if(k == 1){
+            return new Node(ele);
+        } else {
+            return head;
+        }
+    }
+    if(k == 1){
+        Node* newHead = new Node(ele, head); // pointing to new head cuz head is change
+        return newHead;
+    }
+    int cnt = 0;
+    Node* temp = head;
+    while(temp != NULL) {
+        cnt++;
+        if(cnt == (k-1)) {
+            Node* x = new Node(ele, temp->next); // 
+            // ele->next = temp->next; same as x->next = temp->next;
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+// insert before the value
+Node* insertBeforeValue(Node* head, int ele, int val ){
+    if(head == NULL){ // empty list || 
+        cout << "Not found" << endl;
+        return NULL;
+    }
+    if(head->data == val){
+        Node* newHead = new Node(ele, head); // pointing to new head cuz head is change
+        return newHead;
+    }
+    
+    Node* temp = head;
+    bool found = false; // for message
+    while(temp->next != NULL) {
+        if(temp->next->data == val) {
+            Node* x = new Node(ele, temp->next); // 
+            // ele->next = temp->next; same as x->next = temp->next;
+            temp->next = x;
+            found = 1;
+            break;
+        }
+        temp = temp->next;
+    }
+    if(found == false){
+        cout << "Not found" << endl;
+    }
+    return head;
+}
+
 int main(){
     vector<int> arr = {12, 5, 8, 7};
     Node* head = convertArr2LL(arr);
+    Node* newHead = convertArr2LL(arr);
+    Node* newHead2 = convertArr2LL(arr);
+    Node* newHead3 = convertArr2LL(arr);
     cout << "The Linked List : ";
     print(head);
+    
+    head = insertHead(head, 100); // using the insertHead function
+    // head = new Node( 200, head); // directly insert node & val using construction in class
+    cout << endl << "Insert start : ";
+    print(head);
+    
+    newHead = insertTail(newHead, 100);
+    cout << endl << "insert End :";
+    print(newHead);
+    
+    newHead2 = insertKPosition(newHead2, 100, 3);
+    cout << endl << "insert kth position : ";
+    print(newHead2);
+    
+    newHead3 = insertBeforeValue(newHead3, 100, 7);
+    cout << endl << "insert before value : ";
+    print(newHead3);
+}
