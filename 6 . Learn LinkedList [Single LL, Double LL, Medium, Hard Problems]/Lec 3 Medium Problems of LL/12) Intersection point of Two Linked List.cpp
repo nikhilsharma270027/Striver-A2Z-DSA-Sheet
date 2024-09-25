f2150b687b6cdec1e57438be15ac7c07de6a4919
nikhil <nikhilsharma270027@gmail.com>
2024-09-25 19:42:22 +0530

    intersectioninLL2

--- 6 . Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/12) Intersection point of Two Linked List.cpp
+++ 6 . Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3 Medium Problems of LL/12) Intersection point of Two Linked List.cpp
@@ -112,3 +112,42 @@ node* intersectionPresent(node* head1,node* head2) {
 
 // Space Complexity: O(n)
 // Reason: Storing list 1 node addresses in unordered_set.
+
+
+// Better Approach
+int getDifference(node* head1,node* head2) {
+     int len1 = 0,len2 = 0;
+        while(head1 != NULL || head2 != NULL) {
+            if(head1 != NULL) {
+                ++len1; head1 = head1->next;
+            }
+            if(head2 != NULL) {
+                ++len2; head2 = head2->next;
+            }
+            
+        }
+        return len1-len2;//if difference is neg-> length of list2 > length of list1 else vice-versa
+}
+
+//utility function to check presence of intersection
+node* intersectionPresent(node* head1,node* head2) {
+ int diff = getDifference(head1,head2);
+        if(diff < 0) 
+            while(diff++ != 0) head2 = head2->next; 
+        else while(diff-- != 0) head1 = head1->next;
+        while(head1 != NULL) {
+            if(head1 == head2) return head1;
+            head2 = head2->next;
+            head1 = head1->next;
+        }
+        return head1;
+
+
+}
+
+// Time Complexity:
+// O(2max(length of list1,length of list2))+O(abs(length of list1-length of list2))+O(min(length of list1,length of list2))
+// Reason: Finding the length of both lists takes max(length of list1, length of list2) because it is found simultaneously for both of them. Moving the head pointer ahead by a difference of them. The next one is for searching.
+
+// Space Complexity: O(1)
+// Reason: No extra space is used.
