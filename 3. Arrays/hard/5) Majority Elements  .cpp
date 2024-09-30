f94be5812225cb11d91741557d4bc3e6345799cf
nikhil <nikhilsharma270027@gmail.com>
2024-09-30 21:25:51 +0530

    majorityHardarrayoptimal

--- 3. Arrays/hard/5) Majority Elements  .cpp
+++ 3. Arrays/hard/5) Majority Elements  .cpp
@@ -71,6 +71,7 @@ vector<int> majorityElement(vector<int> v) {
     }
     return ls;
 }
+Output: [-1 -1 2 ] [-1 0 1 ]
 Complexity Analysis
 
 Time Complexity: O(N*logN), where N = size of the given array.
@@ -78,3 +79,68 @@ Reason: We are using a map data structure. Insertion in the map takes logN time.
 If we use unordered_map instead, the first term will be O(N) for the best and average case and for the worst case, it will be O(N2).
 
 Space Complexity: O(N) as we are using a map data structure. We are also using a list that stores a maximum of 2 elements. That space used is so small that it can be considered constant.
+
+
+
+// Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++) {
+        //remove duplicates:
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+        //moving 2 pointers:
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k) {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0) {
+                j++;
+            }
+            else if (sum > 0) {
+                k--;
+            }
+            else {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                //skip the duplicates:
+                while (j < k && arr[j] == arr[j - 1]) j++;
+                while (j < k && arr[k] == arr[k + 1]) k--;
+            }
+        }
+    }
+    return ans;
+}
+
+
+int main()
+{
+    vector<int> arr = { -1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    vector<vector<int>> ans = triplet(n, arr);
+    for (auto it : ans) {
+        cout << "[";
+        for (auto i : it) {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
+
+Output: [-1 -1 2 ] [-1 0 1 ]
+Complexity Analysis
+
+Time Complexity: O(NlogN)+O(N2), where N = size of the array.
+Reason: The pointer i, is running for approximately N times. And both the pointers j and k combined can run for approximately N times 
+including the operation of skipping duplicates. So the total time complexity will be O(N2). 
+
+Space Complexity: O(no. of quadruplets), This space is only used to store the answer. We are not using any extra space to solve this problem.
+So, from that perspective, space complexity can be written as O(1).
